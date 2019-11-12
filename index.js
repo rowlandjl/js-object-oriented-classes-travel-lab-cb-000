bedcde04da3c09e5778ab468541cb8d4aebfb7ec
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 01:22:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ class Driver {
   }
 
 }
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
